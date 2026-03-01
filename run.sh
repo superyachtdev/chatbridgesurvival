395924054eb6bafae5facb1173f820fefff2cc60
superyachtdev <zabifierbusiness@gmail.com>
2026-03-01 17:58:28 +0100

    aaaa

--- run.sh
+++ run.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 echo "Starting virtual display..."
+rm -f /tmp/.X99-lock
 Xvfb :99 -screen 0 1024x768x16 &
 export DISPLAY=:99
 
 cd /root/.minecraft
 
-# If fabric loader not installed, install it
-if [ ! -f fabric-loader-1.21.1.jar ]; then
-  echo "Installing Fabric..."
+# Install Fabric CLIENT if not installed
+if [ ! -d versions ]; then
+  echo "Installing Fabric CLIENT..."
   java -jar /app/fabric-installer.jar client \
     -mcversion 1.21.1 \
     -dir /root/.minecraft \
     -downloadMinecraft
 fi
 
-echo "Launching Fabric..."
+echo "Launching Minecraft CLIENT..."
+
 java \
   -Xmx2G \
   -Dorg.lwjgl.opengl.Display.allowSoftwareOpenGL=true \
-  -jar fabric-loader-*.jar
+  -cp "$(echo libraries/*/*.jar | tr ' ' ':'):versions/*/*.jar" \
+  net.fabricmc.loader.impl.launch.knot.KnotClient
